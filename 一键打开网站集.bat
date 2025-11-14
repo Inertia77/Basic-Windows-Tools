ba9faeb811d317fe932e07f25d210879b91a558f
Inertia <153355615+Inertia77@users.noreply.github.com>
2025-11-15 03:29:28 +0900

    Replace URLs with a single Google homepage link
    因为只是为了给个例子，里面的link内容，请自定义

--- 一键打开网站集.bat
+++ 一键打开网站集.bat
@@ -1,4 +1,4 @@
-start "" "https://docs.google.com/spreadsheets/d/1n--FtlOewF6sqbYrXm3pxvVAiOljOWGM3vuDBL78s3c/edit?gid=561381925#gid=561381925"
-start "" "https://drive.google.com/drive/u/0/folders/1LD3-5ScV4tgf3a13cBOHzb-hqkwr_gyS"
-start "" "https://www.notion.so/G-up-29d5adb3775e80f0b766daf9e03ecbd8"
+start "" "https://google.com"
+start "" ""
+start "" ""
 start "" ""
